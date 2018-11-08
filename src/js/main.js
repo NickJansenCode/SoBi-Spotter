8cd7c4c39998ab0114895424fe39e9a27068589b
Nicholas Jansen <000395661@mohawkcollege.ca>
2018-11-08 02:39:15 -0500

    Initial Layout

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+function createMap(position) {
+
+    // Gets the map div from the DOM. //
+    var map = new Microsoft.Maps.Map($("#map")[0], {
+        center: position
+    });
+
+}
+
+// Callback method for the bing API script. //
+function loadMapScenario() {
+
+    navigator.geolocation.getCurrentPosition(
+
+        // Callback function for success in the getCurrentLocation() method. //
+        function (position) {
+            createMap(new Microsoft.Maps.Location(position.coords.latitude, position.coords.longitude));
+        },
+
+        // Callback function for an error in the getCurrentLocation() method. //
+        function (error) {
+            // Uses Mohawk College's location if permission is denied. //
+            createMap(new Microsoft.Maps.Location(43.2387, -79.8881));
+        }
+    )
+}
